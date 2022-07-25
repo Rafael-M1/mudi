ba5587bd8fad8223e86b3e0c97a308d0ec398b17
Rafael-M1 <84039013+Rafael-M1@users.noreply.github.com>
2022-07-25 12:46:55 -0400

    Add files via upload

---
+++ mvc/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,101 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedidos")
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nomeProduto;
+	private Double valorNegociado;
+	private LocalDate dataDaEntrega;
+	@Column(length = 500)
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+	
+	public Pedido() {
+	}
+
+	public Pedido(String nomeProduto, Double valorNegociado, LocalDate dataDaEntrega, String urlProduto,
+			String urlImagem, String descricao) {
+		this.nomeProduto = nomeProduto;
+		this.valorNegociado = valorNegociado;
+		this.dataDaEntrega = dataDaEntrega;
+		this.urlProduto = urlProduto;
+		this.urlImagem = urlImagem;
+		this.descricao = descricao;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Double getValorNegociado() {
+		return valorNegociado;
+	}
+
+	public void setValorNegociado(Double valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public StatusPedido getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+}
